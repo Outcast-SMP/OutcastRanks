782dc92e6128a5a2111f6d1bc8b7851c7211a1aa
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-02-23 16:58:04 +0000

    Update PlayerLeave.java

--- Events/PlayerLeave.java
+++ Events/PlayerLeave.java
@@ -1,15 +1,3 @@
-package me.illusion.outcastranks.Events;
-
-import me.illusion.outcastranks.Permissions.Permissions;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.permissions.PermissionAttachment;
-
-import java.util.UUID;
-
 public class PlayerLeave implements Listener {
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent e) {
